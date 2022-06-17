58c6f93c3d42723c12405fe957c6b61c014b382d
akshaya_b <101893007+akshayabankumidi@users.noreply.github.com>
2022-06-17 23:24:20 +0530

    Time: 426 ms (47.43%), Space: 78.8 MB (45.14%) - LeetHub

--- 1985-find-the-kth-largest-integer-in-the-array/1985-find-the-kth-largest-integer-in-the-array.cpp
+++ 1985-find-the-kth-largest-integer-in-the-array/1985-find-the-kth-largest-integer-in-the-array.cpp
@@ -1,13 +1,29 @@
 class Solution {
 public:
+ 
+    struct comparator {
+        bool operator()(string &s1,string &s2) {
+ 
+           if(s1.length()==s2.length()) {
+                for(int i=0;i<s1.length();i++) {
+                   if(s1[i]!=s2[i]) return s1[i]>s2[i];
+                }
+              
+            }
+            return s1.length()>s2.length();
+        }
+    };
     string kthLargestNumber(vector<string>& nums, int k) {
-        priority_queue<pair<int,string>>pq;
-        for(auto x:nums)
-            pq.push({x.size(),x});
-        while(k>1){
-            pq.pop();
-            k--;
-    }
-        return pq.top().second;
+      
+        priority_queue<string,vector<string>,comparator>q;
+        
+        for(auto it:nums) {
+            q.push(it);
+            if(q.size()>k) {
+                q.pop();
+            }
+        }
+
+        return q.top();
     }
 };
