4c314959fb199b50570d63e7fdfb70802b798299
akshaya_b <101893007+akshayabankumidi@users.noreply.github.com>
2022-08-10 19:21:29 +0530

    Time: 11 ms (70.91%), Space: 13.4 MB (98.17%) - LeetHub

--- 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,24 +1,59 @@
+#define fo(i,a,b) for(int i=a;i<b;i++)
+#define rev(i,a,b) for(int i=a;i>=b;i--)
+#define ll long long
+#define all(x) x.begin(),x.end()
 #define pb push_back
+#define sz(x) int(x.size())
+#define vi vector<int>
+#define vvi vector<vector<int>>
+#define ss second
+#define ff first
 class Solution {
 public:
-    vector<int> searchRange(vector<int>& nums, int target) {
-        int l=0,r=nums.size()-1;
-        int first=-1,last=-1;
-        vector<int>ans;
-        while(l<=r){
-            int mid=l+(r-l)/2;
-            if(nums[mid]==target){first=mid;r=mid-1;}
-            else if(nums[mid]>target){r=mid-1;}
-            else{l=mid+1;}
-        }
-         l=0;r=nums.size()-1;
-          while(l<=r){
-            int mid=l+(r-l)/2;
-            if(nums[mid]==target){last=mid;l=mid+1;}
-            else if(nums[mid]<target){l=mid+1;}
-            else{r=mid-1;}
-        }
-        ans.pb(first);ans.pb(last);
+    bool bs(vi& nums,int t){
+     int low=0,high=sz(nums)-1;
+     int mid;
+     while(low<=high){
+         mid=low+(high-low)/2;
+         if(nums[mid]==t)return true;
+         else if(nums[mid]>t) high=mid-1;
+         else low=mid+1;
+         
+     }
+        return false;
+    }
+    int last(vi& nums,int t){//find last true
+    int low=0,high=sz(nums)-1;
+     int mid,ans=0;
+     while(low<=high){
+         mid=low+(high-low)/2;
+         if(nums[mid]<=t){ans=mid;low=mid+1;}
+         else high=mid-1;
+         
+     }
         return ans;
+    }
+    int first(vi& nums,int t){//first true 
+     int low=0,high=sz(nums)-1;
+     int mid,ans=0;
+     while(low<=high){
+         mid=low+(high-low)/2;
+         if(nums[mid]>=t){ans=mid;high=mid-1;}
+         else low=mid+1;
+         
+     }
+        return ans;
+    }
+    vector<int> searchRange(vector<int>& nums, int target) {
+        //check for target 
+        if(!bs(nums,target))
+            return {-1,-1};
+        else{
+            int l=last(nums,target);
+            int f=first(nums,target);
+            cout<<f<<" "<<l;
+            return {f,l}; 
+            //return {0,0};
+        }
     }
 };
