491b26ba48eeb11c7f47c702be54b6527fb7f480
akshaya_b <101893007+akshayabankumidi@users.noreply.github.com>
2022-08-21 00:04:03 +0530

    Time: 1040 ms (20.00%), Space: 94.1 MB (60.00%) - LeetHub

---
+++ 2381-shifting-letters-ii/2381-shifting-letters-ii.cpp
@@ -0,0 +1,42 @@
+#define fo(i,a,b) for(int i=a;i<b;i++)
+#define rev(i,a,b) for(int i=a;i>=b;i--)
+#define ll long long
+#define all(x) x.begin(),x.end()
+#define pb push_back
+#define sz(x) int(x.size())
+#define vi vector<int>
+#define vvi vector<vector<int>>
+#define ss second
+#define ff first
+class Solution {
+public:
+    string shiftingLetters(string s, vector<vector<int>>& shifts) {
+        vector<int>vec(sz(s)+1);
+        map<char,int>mp;
+        char chr='a';
+        fo(i,0,26){mp[i]=chr;chr++;}
+        fo(i,0,sz(shifts)){
+            if(shifts[i][2]==0){
+              vec[shifts[i][0]]--;
+                vec[shifts[i][1]+1]++;
+            }
+            else{
+                 vec[shifts[i][0]]++;
+                vec[shifts[i][1]+1]--;
+            }
+        }
+        fo(i,1,sz(s)){
+            vec[i]+=vec[i-1];
+        }
+        string ans="";
+        for(auto x: vec)cout<<x<<" ";cout<<"\n";
+        fo(i,0,sz(s)){
+              int x=s[i]-'a';
+              int y=(x+vec[i]%26+26)%26;
+              ans+=mp[y];
+           
+           
+        }
+        return ans;
+    }
+};
